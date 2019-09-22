f88b2fde284f31157c696b6d76e206f639030da6
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-09-22 13:09:14 +0200

    add react router wrapper

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { PrivateRoute } from './PrivateRoute.js';
 import { history } from './helpers';
-import { alertActions } from './actions';
-import { HomePage } from './components/HomePage';
-import { LoginPage } from './components/LoginPage';
-import { RegisterPage } from './components/RegisterPage';
+// import { alertActions } from './actions';
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
 
 export class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        const { dispatch } = this.props;
-        history.listen((location, action) => {
-        });
-    }
+    // const { dispatch } = this.props;
+    history.listen((location, action) => {});
+  }
 
-    render() {
-        const { alert } = this.props;
-        return (
-              <div className="container">
-                  <div className="col-sm-8 col-sm-offset-2">
-                              <LoginPage />
-                  </div>
+  render() {
+    const { alert } = this.props;
+    console.log(alert, '====');
+    return (
+      <Router history={history}>
+        <div className="container">
+          <div className="col-sm-8 col-sm-offset-2">
+            {alert.type === 'alert-danger' && (
+              <div className="alert alert-danger">
+                <p>{alert.message}</p>
               </div>
-        );
-    }
+            )}
+
+            {alert.type === 'alert-success' && (
+              <div className="alert alert-success">
+                <p>{alert.message}</p>
+              </div>
+            )}
+            <Switch>
+              <PrivateRoute exact path="/" component={HomePage} />
+              <Route path="/register" component={RegisterPage} />
+              <Route path="/login" component={LoginPage} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
-function mapStateToProps(state) {
-    const { alert } = state;
-    return {
-        alert
-    };
-}
+const mapStateToProps = ({ alert }) => ({
+  alert
+});
+
+export default connect(mapStateToProps)(App);
