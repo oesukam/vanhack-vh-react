89188a4a0cfb83cf7503a4c7f0d5d52279efa2e2
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-09-22 13:05:29 +0200

    complete alert redux actions

--- src/actions/alert.actions.js
+++ src/actions/alert.actions.js
@@ -1,16 +1,21 @@
 import { alertConstants } from '../constants';
 
+const success = message => ({
+  type: alertConstants.SUCCESS,
+  payload: message
+});
+
+const error = message => ({
+  type: alertConstants.ERROR,
+  payload: message
+});
+
+const clear = () => ({
+  type: alertConstants.CLEAR
+});
+
 export const alertActions = {
-    success,
-    error,
-    clear
+  success,
+  error,
+  clear
 };
-
-function success(message) {
-}
-
-function error(message) {
-}
-
-function clear() {
-}
