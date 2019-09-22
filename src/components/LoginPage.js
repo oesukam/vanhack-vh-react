e2eb47508f35e9a6f5d25f64c85557274c8cc588
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-09-22 14:41:51 +0200

    implement login feature

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,59 +6,85 @@ import { connect } from 'react-redux';
 import { userActions } from '../actions';
 
 export class LoginPage extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        // reset login status
+    // reset login status
 
-        this.state = {
-            username: '',
-            password: '',
-            submitted: false
-        };
+    this.state = {
+      username: '',
+      password: '',
+      submitted: false
+    };
+  }
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value
+    });
+  };
 
-    handleChange(e) {
-    }
+  handleSubmit = e => {
+    e.preventDefault();
+    const { username, password } = this.state;
+    const { dispatch } = this.props;
+    this.setState({ submitted: true });
+    if (!username || !password) return;
+    dispatch(userActions.login(username, password));
+  };
 
-    handleSubmit(e) {
-        
-    }
-
-    render() {
-        const { username, password, submitted } = this.state;
-        return (
-            <div className="col-md-6 col-md-offset-3">
-                <h2>Login</h2>
-                <form name="form">
-                    <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
-                        <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control username" name="username" />
-                        {submitted && !username &&
-                            <div className="help-block">Username is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password"/>
-                        {submitted && !password &&
-                            <div className="help-block">Password is required</div>
-                        }
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary">Login</button>
-                    </div>
-                </form>
-            </div>
-        );
-    }
-}
-
-function mapStateToProps(state) {
-
+  render() {
+    const { username, password, submitted } = this.state;
+    return (
+      <div className="col-md-6 col-md-offset-3">
+        <h2>Login</h2>
+        <form name="form" onSubmit={this.handleSubmit}>
+          <div
+            className={
+              'form-group' + (submitted && !username ? ' has-error' : '')
+            }
+          >
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              className="form-control username"
+              name="username"
+              onChange={this.handleChange}
+            />
+            {submitted && !username && (
+              <div className="help-block">Username is required</div>
+            )}
+          </div>
+          <div
+            className={
+              'form-group' + (submitted && !password ? ' has-error' : '')
+            }
+          >
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              onChange={this.handleChange}
+            />
+            {submitted && !password && (
+              <div className="help-block">Password is required</div>
+            )}
+          </div>
+          <div className="form-group">
+            <button className="btn btn-primary" onClick={this.handleSubmit}>
+              Login
+            </button>
+            <Link to={'/register'} className="btn btn-link">
+              Register
+            </Link>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export { LoginPage as TestLoginPage };
+
+export default connect()(LoginPage);
